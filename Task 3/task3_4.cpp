1da69d30585d0c03bed988b3af43641095d146b9
Aya Tarek <ayatarek18917@gmail.com>
2024-12-20 23:28:19 +0200

    Updated Task 3

--- Task 3/task3_4.cpp
+++ Task 3/task3_4.cpp
@@ -5,24 +5,23 @@ using namespace std;
 //Encrypts a message using a simple cipher algorithm using the Italian alphebet.
 //using a function(x) = ax + b 
 //Affine cipher Keys : a = 5 , b = 5
-string chipered_message(string message)
+string chipered_message(string message , int a , int b)
 {
-    int a, b;
-    int alphabet_size = 21;
+    int alphabet_size = 22;
     vector<int> x;
     vector<int> func_result;
     vector<int> modulo_result;
     string encrypted_message = "";
-    a = 5;
-    b = 5;
-    for (int a{}; a < message.size(); a++)
+    //transering the message to uppercase
+    for (int c{}; c < message.size(); c++)
     {
-        message[a] = toupper(message[a]);
+        message[c] = toupper(message[c]);
     }
-    char italian_alphabet[21] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Z' };
+    char italian_alphabet[22] = { ' ','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Z'};
+    //encoding the message
     for (int i{}; i < message.length(); i++)
     {
-        for (int j{}; j < 21; j++)
+        for (int j{}; j < alphabet_size ; j++)
         {
             if (message[i] == italian_alphabet[j])
                 x.push_back(j);
@@ -30,32 +29,40 @@ string chipered_message(string message)
     }
     func_result.resize(x.size());
     modulo_result.resize(x.size());
+    //Calculating the function and modulos for x vector 
     for (int k{}; k < x.size(); k++)
     {
         func_result[k] = a * x[k] + b;
         modulo_result[k] = func_result[k] % alphabet_size;
     }
+    //Decoding the modulo_result vector into italian charcters 
     for (int u{}; u < message.length(); u++)
     {
         encrypted_message += italian_alphabet[modulo_result[u]];
     }
     return encrypted_message;
 }
+/******** Example Usage **********/
+//argumentes bounded by '*' are the user input in cmd :
+/*
+------------------------------------------------------------------------------------
+Encrypting into the Italian language
+Please enter the message to be ciphered : *STUDIA BENE PER LA FINALE*
+Please enter the Affine cipher keys:* 5 5 *
+-The return value
+The ciphered message : BGNCFLEQHZHEIHUEMLEOFZLMH
+*/
 int main()
 {
     string message;
     string submessage;
-    int index = 0;
+    int a, b;
     string modified_message;
     cout << "Encrypting into the Italian language" << endl;
-    cout << "Please enter the message to be ciphered :";
+    cout << "Please enter the message to be ciphered : ";
     getline(cin, message);
-    for (int i{}; i < message.size(); i++) {
-        if ((message[i] == ' ')|| (i == message.size() - 1)) {
-            submessage = message.substr(index, i - index);
-            index = i + 1;
-            modified_message += chipered_message(submessage) + ' ';
-        }
-    }
-    cout << endl << modified_message;
+    cout << endl << "Please enter the Affine cipher keys: ";
+    cin >> a >> b ;
+    modified_message = chipered_message(message , a , b);
+    cout << endl<< "The ciphered message : " << modified_message;
 }
